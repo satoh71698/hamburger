17cef700ef2e1078d893840dc362043c76f94fd4
Tomoya Satoh <tomo@TomoyanoMacBook-Pro.local>
2023-06-22 13:43:04 +0900

    404修正

--- 404.php
+++ 404.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>
-      <?php get_template_part("content-sideMenu"); ?>
-      <div class="p-textAria">
-     <P>ページが見つかりません</p>
+        <?php get_template_part("content-sideMenu"); ?>
+        <!-- main -->
+        <main>
+            <!-- p-fv -->
+            <div class="p-textAria__archive u-inner--20">
+              <P>ページが見つかりません</p>
      <a href="<?php echo esc_url( home_url( '/' ) ); ?>">トップページはこちら</a>
+            </div>
+        </main>
+        <!-- /main -->
     </div>
 <?php get_footer(); ?>
